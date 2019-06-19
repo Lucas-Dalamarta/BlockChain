a4841607f13af6a8e7c6a717dd8968b58ec8d041
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2019-06-18 21:48:43 -0300

    First commit , work in progress

---
+++ BlockChain.py
@@ -0,0 +1,62 @@
+global BlockChain , Choice 
+BlockChain  =   [[1]]
+Choice = 1
+
+def messages(sample):
+    return  print("Invalid value , please , Try Again")
+
+def formats(text,counts):
+    """Returns a print , given character and how many loops"""
+    printable = ""
+
+    for i in range(counts):
+        printable   += text
+    return  print(printable)
+
+
+def lastValue():
+    """Returns BlockChain last value"""
+    return  BlockChain[-1]
+
+
+def SetValue(new_value):
+    """Receives and appends a new value to the BlockChain"""
+    BlockChain.append([lastValue(),new_value])
+
+
+def getBlockChain():
+    """Returns BlockChain"""
+    return print(BlockChain)
+
+formats("-",60)
+print("\tHello ! Welcome to Luk3's BlockChain")
+formats("-",60)
+
+while Choice != 0:
+
+    print("1 - For new entry :")
+    print("2 - To visualize BlockChain :")
+    print("0 - To quit :")
+    print("\n\n\t")
+    Choice = int(input())
+
+    if Choice == 1 :
+        while True: 
+            try:
+                value = float(input("Enter value :\t"))
+                if value <= 0:
+                    raise ValueError
+            except  ValueError:
+                messages("Error")
+                continue
+            break
+        SetValue(value)
+    elif Choice == 2 :
+        getBlockChain()
+    else:
+        break
+    continue
+
+formats("*",60)
+print("\tThanks for using Luk3's BlockChain")
+formats("*",60)
--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 It's been developed during a *Udemy's* course __Learn Python by Building a Blockchain & Cryptocurrency__
 
-- [x] Start a simple BlockChain
-- [x] Develop functions , to make the __code easier to read__=
+- [x] Start a simple BlockChain 
+- [x] Develop functions , and make the __code easier to read__
 - [ ] Get a more complex and valid BlockChain
 - [ ] Finally submit a valid BlockChain
  
